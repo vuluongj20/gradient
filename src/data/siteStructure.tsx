ed34c353b0defcdfa0ec15b412d7d63552080634
Vu Luong <vuluongj20@gmail.com>
2021-10-16 22:10:16 -0700

    Create siteStructure.tsx

---
+++ src/data/siteStructure.tsx
@@ -0,0 +1,23 @@
+type Page = {
+	id: string
+	title: string
+	path: string
+}
+
+export const columns: Page[] = [
+	{
+		id: 'technology',
+		title: 'Technology',
+		path: '/column/technology',
+	},
+	{
+		id: 'design',
+		title: 'Design',
+		path: '/column/design',
+	},
+	{
+		id: 'law',
+		title: 'Law',
+		path: '/column/law',
+	},
+]
