7256e306741fe5395e4be9e2e59031c75370243c
Vu Luong <vuluongj20@gmail.com>
2021-10-16 21:34:25 -0700

    Separate file for GlobalStyles

---
+++ src/layouts/globalStyles.tsx
@@ -0,0 +1,87 @@
+import { createGlobalStyle } from 'styled-components'
+
+import {
+  sohneAtRules,
+  domaineDisplayNarrowAtRules,
+  domaineTextAtRules,
+} from './theme/fonts'
+
+import { theme } from '@utils'
+
+const GlobalStyles = createGlobalStyle`
+  ${sohneAtRules}
+  ${domaineDisplayNarrowAtRules}
+  ${domaineTextAtRules}
+  html {
+    font-size: ${theme('t.rootSize')};
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  *,
+  *::before,
+  *::after {
+    box-sizing: inherit;
+  }
+  body {
+    font-feature-settings: 'kern', 'liga';
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    direction: ltr;
+    text-align: left;
+    font-family: ${theme('t.ui.body.fontFamily')};
+    font-size: ${theme('t.rootSize')};
+    color: ${theme('c.gray2')};
+    margin: 0;
+    padding: 0;
+  }
+  h1, h2, h3, h4, h5, h6 {
+    color: ${theme('c.gray1')};
+    margin: 0;
+  }
+  h1 {${theme('t.ui.h1')}}
+  h2 {${theme('t.ui.h2')}}
+  h3 {${theme('t.ui.h3')}}
+  h4 {${theme('t.ui.h4')}}
+  h5 {${theme('t.ui.h5')}}
+  h6 {${theme('t.ui.h6')}}
+  p, a {
+    ${theme('t.ui.body')}; 
+    margin: 0;
+  }
+  a {
+    color: ${theme('c.gray2')};
+    text-decoration-color: ${theme('c.gray8')};
+    cursor: pointer;
+    transition: color, box-shadow 0.25s ${theme('a.easeOutQuad')};
+  }
+  a:hover {
+    color: ${theme('c.gray1')};
+    text-decoration-color: ${theme('c.gray5')};
+  }
+  a:focus-visible {
+    ${theme('u.focusVisible')}; 
+  }
+  button {
+    appearance: none;
+    background: transparent;
+    border: none;
+    border-radius: 0.5em;
+
+    ${theme('t.ui.label')};
+    color: ${theme('c.gray1')};
+
+    cursor: pointer;
+
+    transition: color, box-shadow 0.25s ${theme('a.easeOutQuad')};
+
+    &:hover {
+      color: ${theme('c.gray3')};
+    }
+  }
+  button:focus-visible {
+    ${theme('u.focusVisible')}; 
+  }
+`
+
+export default GlobalStyles
--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,101 +1,19 @@
 import { CloudinaryContext } from 'cloudinary-react'
 import { ReactNode, useEffect, useState } from 'react'
-import styled, { createGlobalStyle, ThemeProvider } from 'styled-components'
+import styled, { ThemeProvider } from 'styled-components'
 
 import Footer from './footer'
+import GlobalStyles from './globalStyles'
 import Nav from './nav'
 import { getTheme } from './theme'
-import {
-	sohneAtRules,
-	domaineDisplayNarrowAtRules,
-	domaineTextAtRules,
-} from './theme/fonts'
 
-import { theme, gridColCounts } from '@utils'
+import { gridColCounts } from '@utils'
 import LocalThemeProvider from '@utils/localTheme'
 import SettingsContext, { SettingsProvider } from '@utils/settings'
 
-type Props = {
-	children: ReactNode
-}
+type Props = { children: ReactNode }
 
 const frameWidth = 2 // unit = em
-
-const GlobalStyle = createGlobalStyle`
-	${sohneAtRules}
-	${domaineDisplayNarrowAtRules}
-	${domaineTextAtRules}
-	html {
-	  font-size: ${theme('t.rootSize')};
-	  box-sizing: border-box;
-	  margin: 0;
-	  padding: 0;
-	}
-	*,
-	*::before,
-	*::after {
-	  box-sizing: inherit;
-	}
-  body {
-  	font-feature-settings: 'kern', 'liga';
-  	-webkit-font-smoothing: antialiased;
-  	-moz-osx-font-smoothing: grayscale;
-  	direction: ltr;
-  	text-align: left;
-  	font-family: ${theme('t.ui.body.fontFamily')};
-  	font-size: ${theme('t.rootSize')};
-  	color: ${theme('c.gray2')};
-  	margin: 0;
-  	padding: 0;
-  }
-  h1, h2, h3, h4, h5, h6 {
-  	color: ${theme('c.gray1')};
-  	margin: 0;
-  }
-  h1 {${theme('t.ui.h1')}}
-  h2 {${theme('t.ui.h2')}}
-  h3 {${theme('t.ui.h3')}}
-  h4 {${theme('t.ui.h4')}}
-  h5 {${theme('t.ui.h5')}}
-  h6 {${theme('t.ui.h6')}}
-  p, a {
-  	${theme('t.ui.body')}; 
-  	margin: 0;
-  }
-  a {
-  	color: ${theme('c.gray2')};
-  	text-decoration-color: ${theme('c.gray8')};
-  	cursor: pointer;
-  	transition: color, box-shadow 0.25s ${theme('a.easeOutQuad')};
-  }
-  a:hover {
-  	color: ${theme('c.gray1')};
-  	text-decoration-color: ${theme('c.gray5')};
-  }
-  a:focus-visible {
-  	${theme('u.focusVisible')}; 
-  }
-  button {
-  	appearance: none;
-  	background: transparent;
-  	border: none;
-  	border-radius: 0.5em;
-
-  	${theme('t.ui.label')};
-  	color: ${theme('c.gray1')};
-
-  	cursor: pointer;
-
-  	transition: color, box-shadow 0.25s ${theme('a.easeOutQuad')};
-
-  	&:hover {
-  		color: ${theme('c.gray3')};
-  	}
-  }
-  button:focus-visible {
-  	${theme('u.focusVisible')}; 
-  }
-`
 
 const Layout = ({ children }: Props): JSX.Element => {
 	const [mounted, setMounted] = useState(false)
@@ -113,7 +31,7 @@ const Layout = ({ children }: Props): JSX.Element => {
 				<SettingsContext.Consumer>
 					{({ settings }) => (
 						<ThemeProvider theme={getTheme(settings.theme)}>
-							<GlobalStyle />
+							<GlobalStyles />
 							<Nav frameWidth={frameWidth} />
 							<PageContent id="page-content">{children}</PageContent>
 							<LocalThemeProvider appearance="inverted">
