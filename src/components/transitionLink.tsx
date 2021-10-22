6564768e45dedc8d42bdbc7a32c3b737d19ad898
Vu Luong <vuluongj20@gmail.com>
2021-10-21 23:19:48 -0700

    TransitionLink props

--- src/components/transitionLink.tsx
+++ src/components/transitionLink.tsx
@@ -1,16 +1,27 @@
 import TransitionLink from 'gatsby-plugin-transition-link'
 import { ReactNode } from 'react'
+import styled from 'styled-components'
 
 type Props = {
 	to: string
 	children: ReactNode
-	onExit: () => void
-	className: string
-	tabIndex: number
+	onExit?: () => void
+	onClick?: () => void
+	className?: string
+	tabIndex?: number
+	disabled?: boolean
 }
 
-const Link = ({ to, children, onExit, className, tabIndex = 0 }: Props): JSX.Element => (
-	<TransitionLink
+const Link = ({
+	to,
+	children,
+	onExit,
+	onClick,
+	className,
+	tabIndex = 0,
+	disabled,
+}: Props): JSX.Element => (
+	<StyledTransitionLink
 		to={to}
 		className={className}
 		exit={{
@@ -21,10 +32,23 @@ const Link = ({ to, children, onExit, className, tabIndex = 0 }: Props): JSX.Ele
 			delay: 1,
 			duration: 1,
 		}}
+		onClick={onClick}
 		tabIndex={tabIndex}
+		disabled={disabled}
 	>
 		{children}
-	</TransitionLink>
+	</StyledTransitionLink>
 )
 
 export default Link
+
+const StyledTransitionLink = styled(TransitionLink)<{ disabled: boolean }>`
+	${(p) =>
+		p.disabled &&
+		`
+		p, span {
+			color: ${p.theme.c.label};
+		}
+		pointer-events: none;
+		`}
+`
