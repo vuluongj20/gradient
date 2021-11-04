db7a6643007e3a83acd6811af63748998062b9ca
Vu Luong <vuluongj20@gmail.com>
2021-11-03 20:21:01 -0700

    Optional aspectRatio for <Image />

--- src/components/image.tsx
+++ src/components/image.tsx
@@ -1,24 +1,81 @@
 import { Image as CloudinaryImage } from 'cloudinary-react'
 import styled from 'styled-components'
 
-import { Image as Props } from '@types'
+import { Image } from '@types'
 
-const Image = ({ src, alt }: Props): JSX.Element => {
-	if (process.env.NODE_ENV === 'development') {
-		return <StyledImage src={`../images/${src}`} alt={alt} />
-	}
-
-	return <StyledCloudinaryImage publicId={`gradient/${src}`} alt={alt} />
+type Props = Image & {
+	aspectRatio?: 'square' | 'wide' | 'rect'
 }
 
-export default Image
+const ImageWrap = ({ src, alt, aspectRatio }: Props): JSX.Element => {
+	const getPaddingTop = (aspectRatio: string): string => {
+		switch (aspectRatio) {
+			/** 1:1 */
+			case 'square':
+				return '100%'
+			/** 4:3 */
+			case 'rect':
+				return '75%'
+			/** 16:9 */
+			case 'wide':
+			default:
+				return '56.25%'
+		}
+	}
 
-const StyledImage = styled.img`
+	const paddingTop = aspectRatio ? getPaddingTop(aspectRatio) : null
+
+	if (process.env.NODE_ENV === 'development') {
+		return (
+			<Wrap paddingTop={paddingTop}>
+				<StyledImage src={`../images/${src}`} alt={alt} spread={!!aspectRatio} />
+			</Wrap>
+		)
+	}
+
+	return (
+		<Wrap paddingTop={paddingTop}>
+			<StyledCloudinaryImage
+				publicId={`gradient/${src}`}
+				alt={alt}
+				spread={!!aspectRatio}
+			/>
+		</Wrap>
+	)
+}
+
+export default ImageWrap
+
+const Wrap = styled.div<{ paddingTop?: string }>`
 	display: block;
 	width: 100%;
+	position: relative;
+
+	${(p) => p.paddingTop && `padding-top: ${p.paddingTop};`}
 `
 
-const StyledCloudinaryImage = styled(CloudinaryImage)`
-	display: block;
+const StyledImage = styled.img<{ spread: boolean }>`
 	width: 100%;
+	${(p) =>
+		p.spread &&
+		`
+			position: absolute;
+			height: 100%;
+			top: 0;
+			object-fit: cover;
+			object-position: center;
+		`}
+`
+
+const StyledCloudinaryImage = styled(CloudinaryImage)<{ spread: boolean }>`
+	width: 100%;
+	${(p) =>
+		p.spread &&
+		`
+			position: absolute;
+			height: 100%;
+			top: 0;
+			object-fit: cover;
+			object-position: center;
+		`}
 `
