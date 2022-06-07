55b38db6c4ca85c6577004639d17b079f4125647
Vu Luong <vuluongj20@gmail.com>
2022-06-06 22:55:22 -0700

    feat(utils): Add useResizeObserver

---
+++ src/utils/hooks/useResizeObserver.tsx
@@ -0,0 +1,29 @@
+import { RefObject, useEffect } from 'react'
+import ResizeObserver from 'resize-observer-polyfill'
+
+type Props<T> = {
+  ref: RefObject<T>
+  onResize: () => void
+}
+
+const useResizeObserver = <T extends Element>({ ref, onResize }: Props<T>) => {
+  useEffect(() => {
+    const element = ref.current
+    if (!element) {
+      return
+    }
+
+    const ResizeObserverInstance = new ResizeObserver((entries) => {
+      if (!entries.length) {
+        return
+      }
+      onResize()
+    })
+
+    ResizeObserverInstance.observe(element)
+
+    return element && ResizeObserverInstance.unobserve(element)
+  }, [ref, onResize])
+}
+
+export default useResizeObserver
