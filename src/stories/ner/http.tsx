b4c477643c1453187ba8279ebfd9fdfeeaa8aba9
Vu Luong <vuluongj20@gmail.com>
2022-09-06 20:58:21 -0700

    ref(crf): Update server address

--- src/stories/ner/http.tsx
+++ src/stories/ner/http.tsx
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 const getBaseUrl = () => {
   switch (process.env.NODE_ENV) {
-    // case 'production':
-    //   return 'https://crf-test-reg32b3vxq-uc.a.run.app'
+    case 'production':
+      return 'https://ner-reg32b3vxq-uc.a.run.app'
     case 'development':
     default:
       return 'http://127.0.0.1:5000'
