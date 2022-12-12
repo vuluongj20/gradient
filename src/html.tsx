89a27acc741aaf09ea7789326fa177f84ff08ec1
Vu Luong <vuluongj20@gmail.com>
2022-12-12 09:44:40 -0800

    ref(html): Clean up html.tsx

--- src/html.tsx
+++ src/html.tsx
@@ -2,6 +2,19 @@ import { HTMLAttributes, ReactNode } from 'react'
 
 import { colorAliases } from '@theme/colors'
 
+const paletteStyles = `body{background-color:${
+  colorAliases.paper.background
+}}@media only screen and (prefers-color-scheme:dark){body{background-color:${
+  colorAliases.charcoal.background
+}}}${(Object.keys(colorAliases) as (keyof typeof colorAliases)[])
+  .map(
+    (key) =>
+      `body.palette-${key}{background-color:${colorAliases[key].background}}@media only screen and (prefers-color-scheme:dark){body.palette-${key}{background-color:${colorAliases[key].background}}}`,
+  )
+  .join('')}`
+
+const paletteScript = `!function(){try{const e=JSON.parse(localStorage.getItem("UP")),t=e?.theme?.colors?.appearance,a=e?.theme?.colors?.lightPalette,o=e?.theme?.colors?.darkPalette;if(t&&"auto"!==t){const e="light"===t?a:o;document.body.classList.add("palette-"+e)}else{!0===window.matchMedia("(prefers-color-scheme: dark)").matches?document.body.classList.add("palette-"+o):document.body.classList.add("palette-"+a)}}catch(e){console.warn(e)}}();`
+
 interface HTMLProps {
   htmlAttributes: HTMLAttributes<HTMLHtmlElement>
   headComponents: ReactNode
@@ -22,31 +35,12 @@ const HTML = (props: HTMLProps) => {
           content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover"
         />
         <link rel="stylesheet" href="/katex.min.css" />
-        <style
-          dangerouslySetInnerHTML={{
-            __html: `
-            body{background-color:${
-              colorAliases.paper.background
-            }}@media only screen and (prefers-color-scheme:dark){body{background-color:${
-              colorAliases.charcoal.background
-            }}}${(Object.keys(colorAliases) as (keyof typeof colorAliases)[])
-              .map(
-                (key) =>
-                  `body.palette-${key}{background-color:${colorAliases[key].background}}@media only screen and (prefers-color-scheme:dark){body.palette-${key}{background-color:${colorAliases[key].background}}}`,
-              )
-              .join('')}
-          `,
-          }}
-        />
+        <style>{paletteStyles}</style>
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `!function(){try{const e=JSON.parse(localStorage.getItem("UP")),t=e?.theme?.colors?.appearance,a=e?.theme?.colors?.lightPalette,o=e?.theme?.colors?.darkPalette;if(t&&"auto"!==t){const e="light"===t?a:o;document.body.classList.add("palette-"+e)}else{!0===window.matchMedia("(prefers-color-scheme: dark)").matches?document.body.classList.add("palette-"+o):document.body.classList.add("palette-"+a)}}catch(e){console.warn(e)}}();`,
-          }}
-        />
+        <script>{paletteScript}</script>
         <div
           key={`body`}
           id="___gatsby"
